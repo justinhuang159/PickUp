fbf1bb6f1f6532673a63b2c84536c2a67e39c9d2
Stanley Zeng <sz1911@nyu.edu>
2020-03-12 22:53:19 -0400

    Login Screen proto completed

--- PickUp/PickUp/ViewController.swift
+++ PickUp/PickUp/ViewController.swift
@@ -12,9 +12,22 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        assignbackground()
         // Do any additional setup after loading the view.
     }
-
+    
+    func assignbackground(){
+        let background = UIImage(named: "KobeBG.png")
+        
+        var imageView : UIImageView!
+        imageView = UIImageView(frame: view.bounds)
+        imageView.contentMode =  UIView.ContentMode.scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.image = background
+        imageView.center = view.center
+        view.addSubview(imageView)
+        self.view.sendSubviewToBack(imageView)
+    }
 
 }
 
